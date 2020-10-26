14b0de04c1903ce223ad5e30a5781e50bb161623
Arlesson Menezes <arlesson_10@hotmail.com>
2020-10-26 12:05:59 -0300

    Project Imported, Used or Common product

---
+++ ExeHerancaPolimorfismo/Program.cs
@@ -0,0 +1,57 @@
+﻿using ExeHerancaPolimorfismo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ExeHerancaPolimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= n; i++)
+            {
+                begin:
+                Console.WriteLine($"Product #{i} data:");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char productType = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Price: ");
+
+                double productPrice = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                if(productType == 'c' || productType == 'C')
+                {
+                    list.Add(new Product(productName, productPrice));
+                }
+                else if (productType == 'i' || productType == 'I')
+                {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new ImportedProduct(productName, productPrice, customsFee));
+                }
+                else if (productType == 'u' || productType == 'U')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+                     list.Add(new UsedProduct(productName, productType, manufactureDate));
+                }
+                else
+                {
+                    Console.WriteLine("invalid command");
+                    goto begin;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS:");
+            foreach(Product product in list)
+            {
+                Console.WriteLine(product.PriceTag());
+            }
+        }
+    }
+}
